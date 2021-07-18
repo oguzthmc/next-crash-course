1d7d609084cc08f4bc8a6d520a0991ab67fae044
oguzthmc <47873038+oguzthmc@users.noreply.github.com>
2021-07-19 00:00:02 +0300

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <div>
       <Head>
@@ -11,4 +12,15 @@ export default function Home() {
       <h1>Welcome to Next</h1>
     </div>
   );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
